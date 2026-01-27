9b6ffe68c3e549496dc8479c4a7db41dff26703a
Enrique <eamadob23@fpcoruna.afundacion.org>
2026-01-27 10:00:43 +0100

    Añadido ApiService

--- app/src/main/java/com/example/partycoruna/network/ApiService.java
+++ app/src/main/java/com/example/partycoruna/network/ApiService.java
@@ -4,10 +4,15 @@ import com.example.partycoruna.models.RegisterRequest;
 import com.example.partycoruna.models.RegisterResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ApiService {
 
     @POST("auth/register")
     Call<RegisterResponse> register(@Body RegisterRequest request);
+
+    @GET("users/me/favorites")
+    Call<List<Evento>> getFavoritos(@Header("Authorization") String token);
 }
